dce21dbd90024f737b8d17b4b320db7ed7eac84f
Alex Kory <Skotyalin3@gmail.com>
2023-03-08 18:32:17 +0300

    add api endpoints

--- OrderApiApp/Program.cs
+++ OrderApiApp/Program.cs
@@ -1,8 +1,15 @@
 using Microsoft.Extensions.DependencyInjection;
 using OrderApiApp.Model;
+using OrderApiApp.Model.Entity;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration["DbConnectionString"];
+
+builder.Services.AddTransient<IGenericRepository<Client>, EFGenericRepository<Client>>();
+builder.Services.AddTransient<IGenericRepository<Order>, EFGenericRepository<Order>>();
+builder.Services.AddTransient<IGenericRepository<OrderInfo>, EFGenericRepository<OrderInfo>>();
+builder.Services.AddTransient<IGenericRepository<Product>, EFGenericRepository<Product>>();
 
 builder.Services.AddDbContext<YguckjysContext>();
 
